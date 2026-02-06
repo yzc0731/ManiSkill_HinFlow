f4260d4b1a893bb55ac6b5685624bc0ad1276356
yzc0731 <1360187480@qq.com>
2026-02-06 13:20:08 +0000

    [HinFlow] update

--- mani_skill/agents/robots/panda/__init__.py
+++ mani_skill/agents/robots/panda/__init__.py
@@ -1,3 +1,6 @@
 from .panda import Panda
 from .panda_wristcam import PandaWristCam
 from .panda_stick import PandaStick
+from .panda_pullcubetool import PandaPullCubeTool
+from .panda_placesphere import PandaPlaceSphere
+from .panda_pokecube import PandaPokeCube
